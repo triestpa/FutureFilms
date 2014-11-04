75413df44f5c6a740a530f0474f17a98293bdaf1
triestpa <triestpa@grinnell.edu>
2014-11-04 12:09:15 +0100

    Initial Commit

---
+++ FutureFilms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FutureFilms
+//
+//  Created by Patrick on 11/4/14.
+//  Copyright (c) 2014 Patrick Triest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
